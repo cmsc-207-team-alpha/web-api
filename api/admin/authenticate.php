fbdec99c6c968f70bfe6bba11436e012b079c1c0
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-03-31 10:43:56 +0800

    Update authenticate.php

--- api/admin/authenticate.php
+++ api/admin/authenticate.php
@@ -24,18 +24,20 @@ $input = json_decode(file_get_contents("php://input"));
 if (is_null($input)) {
     Http::ReturnError(400, array('message' => 'Administrator details are empty.'));
 } else {
-    if ((!property_exists($input, 'email')) || !property_exists($input, 'password')) {        
-        Http::ReturnError(400, array('message' => 'Email or password was not provided.'));
+    if ((!property_exists($input, 'email') && !property_exists($input, 'mobile')) || !property_exists($input, 'password')) {        
+        Http::ReturnError(400, array('message' => 'Email / mobile or password was not provided.'));
         return;
     }
 
     try {
         // Create Db object
-        $db = new Db(property_exists($input, 'email') ? 'SELECT * FROM `admin` WHERE email = :email LIMIT 1');
+        $db = new Db(property_exists($input, 'email') ? 'SELECT * FROM `admin` WHERE email = :email LIMIT 1' : 'SELECT * FROM `admin` WHERE mobile = :mobile LIMIT 1');
 
         // Bind parameters
         if (property_exists($input, 'email')) {
             $db->bindParam(':email', property_exists($input, 'email') ? $input->email :null);
+        } else {
+            $db->bindParam(':mobile', property_exists($input, 'mobile') ? $input->mobile : null);
         }
 
         // Execute
@@ -50,7 +52,7 @@ if (is_null($input)) {
                 if(password_verify($input->password, $record['password'])) {
                     Http::ReturnSuccess(array('message' => 'Authentication success.', 'id' => $record['id']));
                 } else {
-                    Http::ReturnError(401, array('message' => 'Invalid email and password.'));
+                    Http::ReturnError(401, array('message' => 'Invalid email / mobile and password.'));
                 }
             }
         }
