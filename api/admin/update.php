5847c0976b041dea936981a03fc3c938701b789c
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-03-31 10:06:30 +0800

    Update update.php

--- api/admin/update.php
+++ api/admin/update.php
@@ -36,7 +36,7 @@ if (is_null($input)) {
             Http::ReturnError(404, array('message' => 'Administrator not found.'));
         } else {
             // Create Db object
-            $db = new Db('UPDATE `admin` SET firstname = :firstname, lastname = :lastname, email = :email, password = :password, WHERE id = :id');
+            $db = new Db('UPDATE `admin` SET firstname = :firstname, lastname = :lastname, email = :email, password = :password, mobile = :mobile, photo = :photo, datemodified = :datemodified WHERE id = :id');
 
             // Bind parameters
             $db->bindParam(':id', property_exists($input, 'id') ? $input->id : 0);
@@ -44,7 +44,9 @@ if (is_null($input)) {
             $db->bindParam(':lastname', property_exists($input, 'lastname') ? $input->lastname : null);
             $db->bindParam(':email', property_exists($input, 'email') ? $input->email : null);
             $db->bindParam(':password', property_exists($input, 'password') ? password_hash($input->password, PASSWORD_DEFAULT) : null);
-
+            $db->bindParam(':mobile', property_exists($input, 'mobile') ? $input->mobile : null);
+            $db->bindParam(':photo', property_exists($input, 'photo') ? $input->photo : null);
+            $db->bindParam(':datemodified', date('Y-m-d H:i:s'));
 
             // Execute
             $db->execute();
