cbb4260911d6253b3a2e85cadb213bba02e38894
turko09 <37707972+turko09@users.noreply.github.com>
2018-03-31 21:03:54 +0800

    Update get.php

--- api/passenger/get.php
+++ api/passenger/get.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting( E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING );
 $dbconfig = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/config/config.ini');
 $host=$dbconfig['db_server'];
 $db=$dbconfig['db_name'];
@@ -7,11 +8,24 @@ $pass=$dbconfig['db_password'];
 $conn=mysqli_connect("$host","$user","$pass","$db");
 
 
+
 if(!isset($_GET['id']))
 {
-	header('HTTP/1.1 400 Bad Request');
-	echo json_encode(array('message' => 'No Value selected'));
-	
+	$get=mysqli_query($conn,"SELECT * FROM passenger");
+	while($sp=mysqli_fetch_array($get))
+	{
+		
+		$passenger->firstname = "$sp[firstname]";
+		$passenger->lastname = "$sp[lastname]";
+		$passenger->mobile = "$sp[mobile]";
+		$passenger->panicmobile = "$sp[panicmobile]";
+		$passenger->address = "$sp[address]";
+		$passenger->email = "$sp[email]";
+		$passenger->password = "$sp[password]";
+		
+		echo json_encode($passenger);
+	}
+	header('HTTP/1.1 201 Request Success');
 }
 else{
 	
@@ -22,7 +36,7 @@ $get=mysqli_query($conn,"SELECT * FROM passenger WHERE id = $id LIMIT 1");
 if(mysqli_num_rows($get)>0)
 {
 		$rv=mysqli_fetch_array($get);
-		$passenger=null;
+		$passenger="";
 		$passenger->firstname = "$rv[firstname]";
 		$passenger->lastname = "$rv[lastname]";
 		$passenger->mobile = "$rv[mobile]";
@@ -44,6 +58,4 @@ else
 
 	
 }
-
-
 ?>
