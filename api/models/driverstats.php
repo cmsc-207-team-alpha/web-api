4b03f603392cdf0a2fe2dde3ed9f7724151ecda2
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-03 00:29:21 +0800

    Update driverstats.php

--- api/models/driverstats.php
+++ api/models/driverstats.php
@@ -5,12 +5,14 @@ class DriverStats
 {
     public $totaldriver;
     public $totalactive;
+    public $totalblocked;
 
     public function __construct(array $data)
     {
         if ($data !== null) {
             $this->totaldriver = (int) $data['totaldriver'] ?? 0;
             $this->totalactive = (int) $data['totalactive'] ?? 0;
+            $this->totalblocked = (int) $data['totalblocked'] ?? 0;
         }
     }
 }
