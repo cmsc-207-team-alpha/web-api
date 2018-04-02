e74e2154418d5a75488bbb2b1ccc2730a42e6ed8
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-04-03 00:13:26 +1000

    Trip rating implementation.

---
+++ api/trip/rate.php
@@ -0,0 +1,72 @@
+<?php
+namespace TeamAlpha\Web;
+
+// Require classes
+require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/http.php';
+
+// Declare use on objects to be used
+use Exception;
+use PDOException;
+
+// Set default response headers
+Http::SetDefaultHeaders('POST');
+
+// Check if request method is correct
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
+    return;
+}
+
+// Extract request body
+$input = json_decode(file_get_contents("php://input"));
+
+if (is_null($input)) {
+    Http::ReturnError(400, array('message' => 'Trip details are empty.'));
+} else {
+    try {
+        // Create Db object
+        $db = new Db('SELECT * FROM `trip` WHERE id = :id LIMIT 1');
+
+        // Bind parameters
+        $db->bindParam(':id', property_exists($input, 'id') ? $input->id : 0);
+
+        // Execute
+        if ($db->execute() === 0) {
+            Http::ReturnError(404, array('message' => 'Trip not found.'));
+        } else {
+            // Create Db object
+            $db = new Db('UPDATE `trip` SET rating = :rating, datemodified = :datemodified WHERE id = :id');
+
+            // Set rating
+            $rating = 3;
+            if (property_exists($input, 'rating')) {
+                if ($input->rating > 5) {
+                    $rating = 5;
+                } else if ($input->rating < 1) {
+                    $rating = 1;
+                } else {
+                    $rating = $input->rating;
+                }
+            }
+
+            // Bind parameters
+            $db->bindParam(':id', property_exists($input, 'id') ? $input->id : 0);
+            $db->bindParam(':rating', $rating);
+            $db->bindParam(':datemodified', date('Y-m-d H:i:s'));
+
+            // Execute
+            $db->execute();
+
+            // Commit transaction
+            $db->commit();
+
+            // Reply with successful response
+            Http::ReturnSuccess(array('message' => 'Trip rated.', 'id' => $input->id));
+        }
+    } catch (PDOException $pe) {
+        Db::ReturnDbError($pe);
+    } catch (Exception $e) {
+        Http::ReturnError(500, array('message' => 'Server error: ' . $e->getMessage() . '.'));
+    }
+}
