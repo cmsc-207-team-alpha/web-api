665795ce762924df76bbdc01aecf600c117b63f0
turko09 <37707972+turko09@users.noreply.github.com>
2018-04-09 00:27:39 +0800

    Update get.php

--- api/passenger/get.php
+++ api/passenger/get.php
@@ -25,7 +25,7 @@ if(!isset($_GET['id']))
 		$passenger->address = "$sp[address]";
 		$passenger->email = "$sp[email]";
 		$passenger->password = "$sp[password]";
-		*/
+		
 		
 		array_push($response, $passenger);
 	
