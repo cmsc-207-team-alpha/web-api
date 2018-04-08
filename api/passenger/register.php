b5422569b4e7ed0cd6c28530857a13d120453a64
turko09 <37707972+turko09@users.noreply.github.com>
2018-04-08 22:28:51 +0800

    Update register.php

--- api/passenger/register.php
+++ api/passenger/register.php
@@ -4,10 +4,10 @@ error_reporting( E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_E
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/email.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/http.php';
+
 // Declare use on objects to be used
 use Exception;
 use PDOException;
-require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/email.php';
 $dbconfig = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/config/config.ini');
 $host=$dbconfig['db_server'];
 $db=$dbconfig['db_name'];
@@ -30,7 +30,7 @@ if(is_null($vals))
 else{
 $firstname=$vals->firstname;
 $lastname=$vals->lastname;
-$email=$vals->email;
+$semail=$vals->email;
 $password=$vals->password;
 $hashed=password_hash("$password", PASSWORD_DEFAULT);	
 $address=$vals->address;
@@ -40,8 +40,8 @@ $datecreated=date("Y-m-d H:i:s");
 $ac=$email." ".$lastname;
 $token=md5($ac);
 
-$checkexisting=mysqli_query($conn, "SELECT email,mobile,panicmobile FROM passenger WHERE email LIKE '$email'
-|| email LIKE '$mobile' || panicmobile LIKE '$pmobile'");
+$checkexisting=mysqli_query($conn, "SELECT email,mobile,panicmobile FROM passenger WHERE email LIKE '$semail'
+|| mobile LIKE '$mobile' || panicmobile LIKE '$pmobile'");
 if(mysqli_num_rows($checkexisting)>0)
 {
 	 header('HTTP/1.1 400 Bad Request');
@@ -54,17 +54,17 @@ if(count(json_decode($data,1))==0) {
     echo json_encode(array('message' => 'Passenger details are empty.'));
 }
 else{
-$remail = $email;
+
 	$name="$lastname, $firstname";
 	$subject="Account Activation";
 	 // Send email
-            $htmlbody = 'Hi ' . $email . ',<br/><br/>Here is your Account Activation Token<br/>' . $token . '<br/><br/>Please do Use this token to activate your account.<br/><br/><br/><small>This message was sent by Team Alpha\'s Passenger Forgot Pass.</small>';
-            $altbody = 'Hi ' .  $email . ', Here is your Account Activation Token ' . $token . ' Please do Use this token to activate your account. This message was sent by Team Alpha Passenger Forgot Pass.';
+            $htmlbody = 'Hi ' . $semail . ',<br/><br/>Here is your Account Activation Token<br/>' . $token . '<br/><br/>Please do Use this token to activate your account.<br/><br/><br/><small>This message was sent by Team Alpha\'s Passenger Forgot Pass.</small>';
+            $altbody = 'Hi ' .  $semail . ', Here is your Account Activation Token ' . $token . ' Please do Use this token to activate your account. This message was sent by Team Alpha Passenger Forgot Pass.';
             $email = new Email();
-            $email->send($remail, $name, 'Temporary Password sent', $htmlbody, $altbody);
+            $email->send($semail, $name, 'Temporary Password sent', $htmlbody, $altbody);
 	
-//Insert Query
 $qrys=mysqli_query($conn, "INSERT INTO passenger VALUES('','$firstname','$lastname',
+//Insert Query
 '$email','$hashed','$address','$mobile','$panicmobile',
 '0','0','0','$token'
 ,'','$datecreated','')")or die("sql error");
