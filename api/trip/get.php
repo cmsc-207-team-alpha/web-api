77364c58829f728c6d408ca4dd820d3d90573729
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-09 16:50:14 +0800

    Add files via upload

--- api/trip/get.php
+++ api/trip/get.php
@@ -23,7 +23,6 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
 $id = 0;
 $vehicleid = 0;
 $stage = '';
-$passengerid = 0;
 
 // Extract request query string
 if (array_key_exists('id', $_GET)) {
@@ -36,12 +35,8 @@ if (array_key_exists('stage', $_GET)) {
     $stage = $_GET['stage'];
 }
 
-if (array_key_exists('passengerid', $_GET)) {
-    $stage = $_GET['passengerid'];
-}
-
-if ($id === 0 && $stage === '' && $passengerid === 0) {
-    Http::ReturnError(400, array('message' => 'Trip id, Passenger id or trip stage was not provided.'));
+if ($id === 0 && $stage === '') {
+    Http::ReturnError(400, array('message' => 'Trip id or trip stage was not provided.'));
     return;
 }
 
@@ -81,9 +76,9 @@ try {
     } else {
         // Create Db object
         $db = new Db('SELECT * FROM `trip` WHERE id = :id LIMIT 1');
+
         // Bind parameters
         $db->bindParam(':id', $id);
-		$db->bindParam(':passengerid', $passengerid);
 
         // Execute
         if ($db->execute() === 0) {
