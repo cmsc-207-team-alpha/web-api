b180f38c21bd974f9e91ce620a278c642fb4f617
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-04-12 10:47:06 +1000

    Get trip history implementation.

--- api/trip/gethistory.php
+++ api/trip/gethistory.php
@@ -19,12 +19,38 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     return;
 }
 
+$driverid = 0;
+$passengerid = 0;
+$conditionSuffix = '';
+// Extract request query string
+if (array_key_exists('driverid', $_GET)) {
+    $driverid = intval($_GET['driverid']);
+    $conditionSuffix = ' AND d.id = :driverid';
+}
+if (array_key_exists('passengerid', $_GET)) {
+    $passengerid = intval($_GET['passengerid']);
+    $conditionSuffix = ' AND p.id = :passengerid';
+}
+
 try {
     // Create Db object
     $db = new Db('SELECT t.*, p.firstname passengerfirstname, p.lastname passengerlastname, v.plateno, v.type, v.make, v.model, v.color, d.firstname driverfirstname, d.lastname driverlastname FROM trip t
-    INNER JOIN passenger p ON t.passengerid = p.id
-    LEFT JOIN vehicle v ON t.vehicleid = v.id
-    LEFT JOIN driver d ON v.driverid = d.id');
+        INNER JOIN passenger p ON t.passengerid = p.id
+        LEFT JOIN vehicle v ON t.vehicleid = v.id
+        LEFT JOIN driver d ON v.driverid = d.id
+        WHERE t.datecreated BETWEEN :datestart AND :dateend' . $conditionSuffix);
+
+    $datestart = array_key_exists('datestart', $_GET) ? $_GET['datestart'] . ' 00:00:00' : '1000-01-01 00:00:00';
+    $dateend = array_key_exists('dateend', $_GET) ? $_GET['dateend'] . ' 23:59:59' : '9999-12-31 23:59:59';
+    // Bind parameters
+    $db->bindParam(':datestart', $datestart);
+    $db->bindParam(':dateend', $dateend);
+    if ($driverid !== 0) {
+        $db->bindParam(':driverid', $driverid);
+    }
+    if ($passengerid !== 0) {
+        $db->bindParam(':passengerid', $passengerid);
+    }
 
     $response = array();
 
