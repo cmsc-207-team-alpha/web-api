08cf6197ff55f1a6922a3fe9852de22231240c6b
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-14 13:56:10 +0800

    Update authenticate.php

--- api/admin/authenticate.php
+++ api/admin/authenticate.php
@@ -1,43 +1,36 @@
 <?php
 namespace TeamAlpha\Web;
-
 // Require classes
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/http.php';
-
 // Declare use on objects to be used
 use Exception;
 use PDOException;
-
 // Set default response headers
 Http::SetDefaultHeaders('POST');
-
 // Check if request method is correct
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
     return;
 }
-
 // Extract request body
 $input = json_decode(file_get_contents("php://input"));
-
 if (is_null($input)) {
     Http::ReturnError(400, array('message' => 'Administrator details are empty.'));
 } else {
-    if ((!property_exists($input, 'email') && !!property_exists($input, 'password')) {        
-        Http::ReturnError(400, array('message' => 'Email or password was not provided.'));
+    if ((!property_exists($input, 'email') && !property_exists($input, 'mobile')) || !property_exists($input, 'password')) {        
+        Http::ReturnError(400, array('message' => 'Email / mobile or password was not provided.'));
         return;
     }
-
     try {
         // Create Db object
         $db = new Db(property_exists($input, 'email') ? 'SELECT * FROM `admin` WHERE email = :email LIMIT 1' : 'SELECT * FROM `admin` WHERE mobile = :mobile LIMIT 1');
-
         // Bind parameters
         if (property_exists($input, 'email')) {
             $db->bindParam(':email', property_exists($input, 'email') ? $input->email :null);
+        } else {
+            $db->bindParam(':mobile', property_exists($input, 'mobile') ? $input->mobile : null);
         }
-
         // Execute
         if ($db->execute() === 0) {
             Http::ReturnError(404, array('message' => 'Administrator not found.'));
