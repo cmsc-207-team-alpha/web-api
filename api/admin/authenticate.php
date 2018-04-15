7a95b3cbe42ab3cc180c0eec021e343dec4421b6
Angelito Sardez, Jr <jojosardez@gmail.com>
2018-04-15 19:38:17 +1000

    Added sessions to be used by web portal.

--- api/admin/authenticate.php
+++ api/admin/authenticate.php
@@ -1,20 +1,28 @@
 <?php
 namespace TeamAlpha\Web;
+
+session_start();
+
 // Require classes
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/http.php';
+
 // Declare use on objects to be used
 use Exception;
 use PDOException;
+
 // Set default response headers
 Http::SetDefaultHeaders('POST');
+
 // Check if request method is correct
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
     return;
 }
+
 // Extract request body
 $input = json_decode(file_get_contents("php://input"));
+
 if (is_null($input)) {
     Http::ReturnError(400, array('message' => 'Administrator details are empty.'));
 } else {
@@ -22,15 +30,18 @@ if (is_null($input)) {
         Http::ReturnError(400, array('message' => 'Email / mobile or password was not provided.'));
         return;
     }
+
     try {
         // Create Db object
         $db = new Db(property_exists($input, 'email') ? 'SELECT * FROM `admin` WHERE email = :email LIMIT 1' : 'SELECT * FROM `admin` WHERE mobile = :mobile LIMIT 1');
+
         // Bind parameters
         if (property_exists($input, 'email')) {
             $db->bindParam(':email', property_exists($input, 'email') ? $input->email :null);
         } else {
             $db->bindParam(':mobile', property_exists($input, 'mobile') ? $input->mobile : null);
         }
+
         // Execute
         if ($db->execute() === 0) {
             Http::ReturnError(404, array('message' => 'Administrator not found.'));
@@ -38,6 +49,8 @@ if (is_null($input)) {
             // Administrator was found
             $record = $db->fetchAll()[0];
                 if(password_verify($input->password, $record['password'])) {
+                    $_SESSION["admin_id"] = $record['id'];
+                    $_SESSION["admin_name"] = $record['firstname'] . ' '. $record['lastname'];
                     Http::ReturnSuccess(array('message' => 'Authentication success.', 'id' => $record['id']));
                 } else {
                     Http::ReturnError(401, array('message' => 'Invalid email / mobile and password.'));
