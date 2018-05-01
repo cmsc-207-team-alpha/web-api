23ca9e7bc0b128b7c97a1ffca77361118c337141
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-05-02 07:54:24 +0800

    Update forgotpass.php

--- api/admin/forgotpass.php
+++ api/admin/forgotpass.php
@@ -1,6 +1,7 @@
 <?php
 namespace TeamAlpha\Web;
 // Require classes
+require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/auth.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/email.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/utils/http.php';
@@ -11,6 +12,13 @@ use Exception;
 use PDOException;
 // Set default response headers
 Http::SetDefaultHeaders('POST');
+
+// Check API Key
+if (!Auth::Authenticate()) {
+    Http::ReturnError(401, array('message' => 'Invalid API Key provided.'));    
+    return;
+}
+
 // Check if request method is correct
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
